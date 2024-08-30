337faa6d254a13d2bf92446527b23d5376e6059f
Anne-Catherine Michaud <annecatherine.michaud@gmail.com>
2024-08-30 22:08:40 +0200

    hotfix CORS

--- src/main/java/com/filrougeapp/config/SecurityConfig.java
+++ src/main/java/com/filrougeapp/config/SecurityConfig.java
@@ -63,7 +63,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080")); //
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080",
+                "http://localhost:8081")); //
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
