4d21cd42cce49b3c48c22b08dfd5e2dbc9ca4381
Nolan-ramos <nolan.ramos.01@gmail.com>
2024-09-02 13:26:59 +0200

    modif modele et controller

---
+++ src/main/java/com/filrougeapp/repository/RaceImgRepository.java
@@ -0,0 +1,7 @@
+package com.filrougeapp.repository;
+
+import com.filrougeapp.model.RaceImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RaceImgRepository extends JpaRepository<RaceImg, Integer> {
+}
