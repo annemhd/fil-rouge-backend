52f964b7b67d3550069135ff62e79b89cbfc09e5
Anne-Catherine Michaud <annecatherine.michaud@gmail.com>
2024-09-02 12:01:12 +0200

    hotfix CORS

--- config.sh
+++ config.sh
@@ -5,8 +5,5 @@ mvn clean package
 # Attendre 20 seconds avant que l'application Spring Boot se lance pour permettre que mysql se lance avant
 sleep 20
 
-
-
-
 # Lancer l'application Spring Boot
-java -jar /app/app.jar
+java -jar ./target/fil-rouge-app-1.0.0.jar
